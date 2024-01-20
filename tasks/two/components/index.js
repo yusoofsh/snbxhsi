d471124618c2c5beab4ab63472b5952b3a51f85f
Yusoof Moh <me@yusoofsh.id>
2024-01-21 01:10:46 +0700

    feat(two): allow jsx usage with Babel

--- tasks/two/components/index.js
+++
@@ -1,5 +0,0 @@
-import { createElement } from "https://esm.sh/react@18.2.0";
-
-export default ({ name }) => {
-  return createElement("h1", { className: "greeting" }, `Halo ${name}`);
-};
--- tasks/two/script.js
+++
@@ -1,6 +0,0 @@
-import { createElement } from "https://esm.sh/react@18.2.0";
-import { createRoot } from "https://esm.sh/react-dom@18.2.0/client";
-import App from "./components/index.js";
-
-const root = createRoot(document.getElementById("root"));
-root.render(createElement(App, { name: "HSI" }));
